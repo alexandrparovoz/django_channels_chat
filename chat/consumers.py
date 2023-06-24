32c495812b71a9fcb5cb3f32942106f5947a9f4d
alex <al.koval2023@gmail.com>
2023-06-24 14:46:24 +0300

    mychat

--- chat/consumers.py
+++ chat/consumers.py
@@ -13,37 +13,38 @@ class ChatConsumer(WebsocketConsumer):
         self.room_name = None
         self.room_group_name = None
         self.room = None
-        self.user = None
-        self.user_inbox = None
+        self.user = None  # для аутентификации юзера
+        self.user_inbox = None  # для личных сообщений
 
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
         self.room = Room.objects.get(name=self.room_name)
-        self.user = self.scope['user']
-        self.user_inbox = f'inbox_{self.user.username}'
+        self.user = self.scope['user']  # для аутентификации юзера
+        self.user_inbox = f'inbox_{self.user.username}' # для личных сообщений
 
-        # connection has to be accepted
+
+        # принятие соединения
         self.accept()
 
-        # join the room group
+        # добавление комнаты в группу
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name,
         )
-        # send the user list to the newly joined user
+        # показывает новому юзеру список юзеров в комнате
         self.send(json.dumps({
             'type': 'user_list',
             'users': [user.username for user in self.room.online.all()],
         }))
 
         if self.user.is_authenticated:
-            # create a user inbox for private messages
+            # для личных сообщений
             async_to_sync(self.channel_layer.group_add)(
                 self.user_inbox,
                 self.channel_name,
             )
-            # send the join event to the room
+            # показыват нового юзера в комнате
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
                 {
@@ -59,12 +60,13 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             self.channel_name,
         )
+        # для личных сообщений
         if self.user.is_authenticated:
             async_to_sync(self.channel_layer.group_discard)(
                 self.user_inbox,
                 self.channel_name,
             )
-            # send the leave event to the room
+            # показывает. что юзер ушел
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
                 {
@@ -76,27 +78,27 @@ class ChatConsumer(WebsocketConsumer):
 
 
     def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        text_data_json = json.loads(text_data) # распарсили
+        message = text_data_json['message'] # извлекли только message
 
         if not self.user.is_authenticated:
             return
-
+        # для личных сообщений
         if message.startswith('/pm '):
             split = message.split(' ', 2)
             target = split[1]
             target_msg = split[2]
 
-            # send private message to the target
+            # для личных сообщений кому
             async_to_sync(self.channel_layer.group_send)(
                 f'inbox_{target}',
                 {
                     'type': 'private_message',
-                    'user': self.user.username,
+                    'user': self.user.username,  # для аутентификации юзера
                     'message': target_msg,
                 }
             )
-            # send private message delivered to the user
+            # доставка  личное сообщение
             self.send(json.dumps({
                 'type': 'private_message_delivered',
                 'target': target,
@@ -104,19 +106,21 @@ class ChatConsumer(WebsocketConsumer):
             }))
             return
 
-            # send chat message event to the room
+            # доставка сообщения в комнату
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type': 'chat_message',
+                'type': 'chat_message',  # добавляем метод для удобоваримости послания
                 'user': self.user.username,
                 'message': message,
             }
         )
         Message.objects.create(user=self.user, room=self.room, content=message)
 
+    # методы для перевода к удобную форму
     def chat_message(self, event):
         self.send(text_data=json.dumps(event))
+
 
     def user_join(self, event):
         self.send(text_data=json.dumps(event))
